9a10e001a9218e7a09e164e93f7a8c73a6012409
Bogdan Nikolov <84232456+bnn16@users.noreply.github.com>
2025-08-03 21:59:45 +0300

    chore: new chad neovim config with 1 file
    using builtin package manager like a chad

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,175 +1,284 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- Leader key
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- relative line numbers for easier nav
+-- Options
 vim.opt.number = true
 vim.opt.relativenumber = true
-
--- spaces instead of tabs
-vim.opt.expandtab = true
-
--- we are not in 1950 use color B)
+vim.opt.wrap = false
+vim.opt.tabstop = 4
+vim.opt.swapfile = false
 vim.opt.syntax = "enable"
 vim.opt.termguicolors = true
-
--- smart indent 
-vim.opt.smartindent = true
-vim.opt.autoindent = true
-
--- line wrapping so we dont scroll vertically
-vim.opt.wrap = true
-
--- Ignore case if the search pattern is all lowercase, be case-sensitive otherwise
-vim.opt.smartcase = true
-vim.opt.ignorecase = true
-
--- list all chars like spaces, tabs, whitespaces and etc.
+vim.opt.winborder = "rounded"
 vim.opt.list = true
-vim.opt.listchars = "tab:.>,trail:.,nbsp:_,space:·"
-
--- always have at least 20 rows visible
+vim.opt.listchars = "tab:..,trail:.,nbsp:_,space:·"
 vim.opt.scrolloff = 20
-
--- use same copy paste buffer as system
 vim.opt.clipboard = "unnamedplus"
-
 vim.opt.signcolumn = 'yes'
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.undofile = true
 
--- if it's a js related use 4, instead of 8
-vim.api.nvim_create_autocmd({"FileType"}, {
-        pattern = {"javascript", "typescript", "typescriptreact", "javascriptreact"},
-        callback = function()
-                vim.opt.tabstop = 4
-                vim.opt.shiftwidth = 4
-                vim.opt.softtabstop = 4
-        end,
+-- Plugins
+vim.pack.add({
+	{ src = "https://github.com/namrabtw/rusty.nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/williamboman/mason.nvim" },
+	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
+	{ src = "https://github.com/hrsh7th/nvim-cmp" },
+	{ src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
+	{ src = "https://github.com/hrsh7th/cmp-buffer" },
+	{ src = "https://github.com/folke/flash.nvim" },
 })
 
-require("nvim-tree").setup({
-        view={
-                side="left"
-        }
+vim.cmd('packloadall')
+
+-- Plugin configuration
+require("mini.pick").setup()
+require("oil").setup()
+require("mason").setup()
+require("gitsigns").setup({
+	current_line_blame = true,
 })
 
-require("gitsigns").setup {
-        current_line_blame = true,
-}
-
-require('nightfox').setup({
-        options = {
-                styles = {
-                        comments = "italic",
-                        keywords = "bold",
-                       types = "italic,bold",
-              }
-     }
+require("flash").setup({
+	labels = "asdfghjklqwertyuiopzxcvbnm",
+	search = {
+		multi_window = true,
+		forward = true,
+	},
+	jump = {
+		jumplist = true,
+		pos = "start",
+		history = false,
+		register = false,
+	},
+	modes = {
+		search = {
+			enabled = true,
+		},
+		char = {
+			enabled = true,
+			jump_labels = true,
+		},
+	},
 })
 
+local cmp = require('cmp')
+cmp.setup({
+	completion = {
+		autocomplete = { cmp.TriggerEvent.TextChanged },
+	},
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'buffer' },
+	}),
+	mapping = cmp.mapping.preset.insert({
+		['<Tab>'] = cmp.mapping.select_next_item(),
+		['<S-Tab>'] = cmp.mapping.select_prev_item(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
+		['<C-Space>'] = cmp.mapping.complete(),
+	}),
+})
 
-require'nvim-treesitter.configs'.setup {
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript","typescript", "markdown", "markdown_inline" },
+-- LSP setup
+local lspconfig = require('lspconfig')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-        sync_install = false,
+lspconfig.lua_ls.setup({ capabilities = capabilities })
+lspconfig.ts_ls.setup({ capabilities = capabilities })
+lspconfig.eslint.setup({ capabilities = capabilities })
+lspconfig.clangd.setup({ capabilities = capabilities })
 
-        auto_install = true,
+-- General keymaps
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 
-        highlight = {
-                enable = true
-        },
-}
+-- File navigation
+vim.keymap.set('n', '<leader>pf', ":Pick files<CR>")
+vim.keymap.set('n', '<leader>pg', ":Pick files tool='git'<CR>")
+vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
+vim.keymap.set('n', '<leader>e', ":Oil<CR>")
 
--- Reserve a space in the gutter
--- This will avoid an annoying layout shift in the screen
-vim.opt.signcolumn = 'yes'
+-- Movement
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
 
-vim.cmd [[packadd packer.nvim]]
+-- Visual mode
+vim.keymap.set("v", "<leader>rn", "\"hy:%s/<C-r>h//g<left><left>")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
 
-return require('packer').startup(function(use)
-        -- colorscheme 
-        use "EdenEast/nightfox.nvim"
-        use "ashen-org/ashen.nvim"
-        use 'cryptomilk/nightcity.nvim'
+-- Window management
+vim.keymap.set('n', '<C-Up>', ':resize +2<CR>')
+vim.keymap.set('n', '<C-Down>', ':resize -2<CR>')
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>')
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>')
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
 
-        -- status bar
-        use({
-                "arsham/arshamiser.nvim",
-                requires = {
-                        "arsham/arshlib.nvim",
-                        "famiu/feline.nvim",
-                        "rebelot/heirline.nvim",
-                        "kyazdani42/nvim-web-devicons",
-                        "lewis6991/gitsigns.nvim",
-                },
-                config = function()
-                        require("arshamiser.feliniser")
-                        vim.cmd("colorscheme nordfox")
-                        --vim.cmd("colorscheme ashen")
-                        --vim.cmd.colorscheme('nightcity')
+-- Flash navigation
+vim.keymap.set({ "n", "x", "o" }, "s", function() require("flash").jump() end)
+vim.keymap.set({ "n", "x", "o" }, "S", function() require("flash").treesitter() end)
+vim.keymap.set({ "o", "x" }, "r", function() require("flash").treesitter_search() end)
+vim.keymap.set({ "c" }, "<c-s>", function() require("flash").toggle() end)
 
-                        _G.custom_foldtext = require("arshamiser.folding").foldtext
-                        vim.opt.foldtext = "v:lua.custom_foldtext()"
+-- Terminal
+vim.keymap.set('n', '<leader>tf', function()
+	local buf = vim.api.nvim_create_buf(false, true)
+	local width = math.floor(vim.o.columns * 0.8)
+	local height = math.floor(vim.o.lines * 0.8)
+	local row = math.floor((vim.o.lines - height) / 2)
+	local col = math.floor((vim.o.columns - width) / 2)
 
-                        vim.api.nvim_set_option("tabline", [[%{%v:lua.require("arshamiser.tabline").draw()%}]])
-                end,
-        })
+	vim.api.nvim_open_win(buf, true, {
+		relative = 'editor',
+		width = width,
+		height = height,
+		row = row,
+		col = col,
+		style = 'minimal',
+		border = 'rounded',
+	})
 
-        -- nvim v0.7.2
-        use({
-                "kdheepak/lazygit.nvim",
-                -- optional for floating window border decoration
-                requires = {
-                        "nvim-lua/plenary.nvim",
-                },
-        })
-
-        -- telescope 
-        use {
-                'nvim-telescope/telescope.nvim', tag = '0.1.8',
-                requires = { {'nvim-lua/plenary.nvim'} }
-        }
-
-        -- git blame line changes
-        use "lewis6991/gitsigns.nvim"
-
-        -- treesitter
-        use(
-        "nvim-treesitter/nvim-treesitter", {run = ":TSUpdate"}
-        )
-
-        -- :TSPlaygroundToggle get ast 
-        use "nvim-treesitter/playground"
-
-        use "theprimeagen/harpoon"
-
-        use "mbbill/undotree"
-        use({'neovim/nvim-lspconfig'})
-        use({'hrsh7th/nvim-cmp'})
-        use({'hrsh7th/cmp-nvim-lsp'})
-
-        use {
-                'nvim-tree/nvim-tree.lua',
-                requires = {
-                        'nvim-tree/nvim-web-devicons', -- optional
-                },
-        }
-
-        use {
-                'VonHeikemen/lsp-zero.nvim',
-                branch = 'v2.x',
-                requires = {
-                        {'neovim/nvim-lspconfig'},
-                        {'williamboman/mason.nvim'},
-                        {'williamboman/mason-lspconfig.nvim'},
-                        {'hrsh7th/nvim-cmp'},
-                        {'hrsh7th/cmp-buffer'},
-                        {'hrsh7th/cmp-path'},
-                        {'saadparwaiz1/cmp_luasnip'},
-                        {'hrsh7th/cmp-nvim-lsp'},
-                        {'hrsh7th/cmp-nvim-lua'},
-                        {'rafamadriz/friendly-snippets'},
-                        {'L3MON4D3/LuaSnip'},
-                }
-        }
+	vim.cmd('terminal')
+	vim.cmd('startinsert')
 end)
+
+-- Autocmds
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(ev)
+		local opts = { buffer = ev.buf }
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+		vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
+		vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+		vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+		vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	callback = function()
+		vim.lsp.buf.format()
+	end,
+})
+
+vim.api.nvim_create_autocmd('VimEnter', {
+	callback = function()
+		local root_markers = { '.git', 'package.json', 'Cargo.toml', 'go.mod' }
+		local root = vim.fs.dirname(vim.fs.find(root_markers, { upward = true })[1])
+		if root then vim.cmd('cd ' .. root) end
+	end
+})
+
+vim.api.nvim_create_autocmd({ 'FocusLost', 'BufLeave' }, {
+	callback = function()
+		if vim.bo.modified and vim.bo.buftype == '' then
+			vim.cmd('silent! write')
+		end
+	end
+})
+
+-- Colorscheme
+vim.cmd("colorscheme rusty")
+
+-- Cheatsheet function
+local function show_cheatsheet()
+	local cheatsheet = {
+		"━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━",
+		"                              🚀 NEOVIM CHEATSHEET 🚀                              ",
+		"━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━",
+		"",
+		"📁 FILE NAVIGATION",
+		"  <leader>pf     Pick files",
+		"  <leader>pg     Pick git files",
+		"  <leader>h      Pick help",
+		"  <leader>e      Oil file explorer",
+		"",
+		"⚡ FLASH NAVIGATION",
+		"  s              Flash jump anywhere",
+		"  S              Flash treesitter (functions, classes)",
+		"  r              Flash treesitter search",
+		"  <C-s>          Toggle flash in search mode",
+		"",
+		"🔧 GENERAL",
+		"  <leader>o      Update & source config",
+		"  <leader>lf     Format buffer",
+		"  <leader>tf     Floating terminal",
+		"",
+		"🚀 MOVEMENT",
+		"  <C-d>          Page down (centered)",
+		"  <C-u>          Page up (centered)",
+		"",
+		"👁️ VISUAL MODE",
+		"  <leader>rn     Search/replace selection",
+		"  J              Move selection down",
+		"  K              Move selection up",
+		"  <              Indent left (stay in visual)",
+		"  >              Indent right (stay in visual)",
+		"",
+		"🪟 WINDOW MANAGEMENT",
+		"  <C-h/j/k/l>    Navigate windows",
+		"  <C-↑/↓/←/→>    Resize windows",
+		"",
+		"🔍 LSP (when attached)",
+		"  K              Hover info",
+		"  gd             Go to definition",
+		"  <leader>d      Show diagnostics",
+		"  ]d / [d        Next/prev diagnostic",
+		"  <leader>q      Diagnostics list",
+		"",
+		"💡 COMPLETION",
+		"  <Tab>          Next completion",
+		"  <S-Tab>        Previous completion",
+		"  <CR>           Confirm completion",
+		"  <C-Space>      Trigger completion",
+		"",
+		"━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━",
+		"                              Press 'q' or <Esc> to close                              ",
+		"━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━",
+	}
+
+	-- Create buffer
+	local buf = vim.api.nvim_create_buf(false, true)
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, cheatsheet)
+	vim.api.nvim_buf_set_option(buf, 'modifiable', false)
+	vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+
+	-- Calculate window size
+	local width = 80
+	local height = #cheatsheet + 2
+	local row = math.floor((vim.o.lines - height) / 2)
+	local col = math.floor((vim.o.columns - width) / 2)
+
+	-- Create window
+	local win = vim.api.nvim_open_win(buf, true, {
+		relative = 'editor',
+		width = width,
+		height = height,
+		row = row,
+		col = col,
+		style = 'minimal',
+		border = 'rounded',
+		title = ' Cheatsheet ',
+		title_pos = 'center',
+	})
+
+	vim.api.nvim_win_set_option(win, 'winhl', 'Normal:Normal,FloatBorder:FloatBorder')
+
+	vim.api.nvim_buf_set_keymap(buf, 'n', 'q', ':close<CR>', { noremap = true, silent = true })
+	vim.api.nvim_buf_set_keymap(buf, 'n', '<Esc>', ':close<CR>', { noremap = true, silent = true })
+end
+
+-- Command to show cheatsheet
+vim.api.nvim_create_user_command('Cheatsheet', show_cheatsheet, {})
+
+-- Keymap to show cheatsheet
+vim.keymap.set('n', '<leader>?', show_cheatsheet, { desc = 'Show cheatsheet' })
